4c9d832b52739ae6271b9c20e78a21452dccd58b
Krzysztof Zalewski <zalewski2010@gmail.com>
2022-10-10 13:50:06 +0100

    Task_2_MainExtended

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,5 +1,6 @@
 public class FirstClass { //
 public static void main (String[] args){ //
-System.out.println("Hello from FirstClass!"); //
+    String example = "Hello from FirstClass!";
+System.out.println(example); //
 }
 }
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+public static void main (String[] args) {
+    String myBrand = "Renault";
+    System.out.println (myBrand);
+    String myName = "Megane";
+    System.out.println (myName);
+    int productionYear = 2008;
+    System.out.println (productionYear);
+    double width = 1.79;
+    System.out.println (width);
+    char carClass = 'B';
+    System.out.println (carClass);
+    boolean isGoodCar = true;
+    System.out.println (isGoodCar);
+}
+}
