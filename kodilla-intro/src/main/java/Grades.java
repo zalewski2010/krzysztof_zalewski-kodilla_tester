393c027fd3e95753749962f5fed92c780c498f9b
Krzysztof Zalewski <zalewski2010@gmail.com>
2022-10-21 11:44:49 +0200

    task_Grades

---
+++ kodilla-intro/src/main/java/Grades.java
@@ -0,0 +1,27 @@
+public class Grades {
+    private int[] grades;
+    private int size;
+
+    public Grades() {
+        this.grades = new int[10];
+        this.size = 0;
+    }
+    public void add (int value) {
+        if (this.size == 10) {
+            return;
+        }
+        this.grades[this.size] = value;
+        this.size++;
+    }
+
+    public int lastGrade() {
+        return this.grades[this.size -1];
+    }
+    public double averageGrades() {
+        double sum = 0;
+        for(int i=0; i < this.size; i++) {
+            sum = sum + this.grades[i];
+        }
+        return sum / this.size;
+        }
+        }
