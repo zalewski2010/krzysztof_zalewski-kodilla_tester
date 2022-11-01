c8e3c49366c6ea3d8e1ab9201a6e2af1228bc4f9
Krzysztof Zalewski <zalewski2010@gmail.com>
2022-11-01 22:37:29 +0100

    1.11.2022_task_3.8_Bank_CashMachine

--- kodilla-basic-tests/src/test/java/com/kodilla/bank/homework/CashMachineTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/bank/homework/CashMachineTestSuite.java
@@ -1,6 +1,7 @@
 package com.kodilla.bank.homework;
 
 import org.junit.jupiter.api.Test;
+import school.Student;
 
 import static com.kodilla.bank.homework.CashMachine.transactionsIncome;
 import static com.kodilla.bank.homework.CashMachine.transactionsOutcome;
@@ -19,29 +20,42 @@ public class CashMachineTestSuite {
     private Double getBalance() {
         return null;
     }
-@Test
+
+    @Test
     public void getAverageAddTransactions() {
-    CashMachine cashMachine = new CashMachine();
-    double sumIn=200;
-    double averageAdd = sumIn/transactionsIncome.length;
-    assertEquals(20,averageAdd);
-}
-@Test
-public void getAverageWithdrawTransactions() {
         CashMachine cashMachine = new CashMachine();
-        double sumOut=-100;
-        double averageWithdraw = sumOut/transactionsOutcome.length;
-    assertEquals(-10,averageWithdraw);
+        double sumIn = 200;
+        double averageAdd = sumIn / transactionsIncome.length;
+        assertEquals(20, averageAdd);
+    }
 
-}
+    @Test
+    public void getAverageWithdrawTransactions() {
+        CashMachine cashMachine = new CashMachine();
+        double sumOut = -100;
+        double averageWithdraw = sumOut / transactionsOutcome.length;
+        assertEquals(-10, averageWithdraw);
 
-@Test
+    }
+
+    @Test
     public void getCountTransactions() {
         CashMachine cashMachine = new CashMachine();
-                double countTransactions = transactionsIncome.length+transactionsOutcome.length;
-    assertEquals(20,countTransactions);
-}
+        double countTransactions = transactionsIncome.length + transactionsOutcome.length;
+        assertEquals(20, countTransactions);
+    }
 
+    @Test
+    public void shouldCalculateAverage() {
+        CashMachine cashMachine = new CashMachine();
+        cashMachine.addTransaction(153);
+        assertEquals(15.30, cashMachine.getAverageAddTransaction(), 0.01);
+    }
 
-
+    @Test
+    public void getCountAddTransaction() {
+        CashMachine cashMachine = new CashMachine();
+        int getCountAddTransaction = transactionsIncome.length;
+        assertEquals(11, getCountAddTransaction);
+    }
 }
